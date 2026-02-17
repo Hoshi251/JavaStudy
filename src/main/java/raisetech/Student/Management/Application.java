ba4e5c58563ea9f59f155ffdd8915bd95a10ac8c
Hoshi251 <xiaotailangxing5@gmail.com>
2026-02-17 10:28:08 +0900

    23課題再コミット

--- src/main/java/raisetech/Student/Management/Application.java
+++ src/main/java/raisetech/Student/Management/Application.java
@@ -1,31 +1,11 @@
 package raisetech.Student.Management;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
-
-	@Autowired
-	private StudentRepository repository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@GetMapping("/studentList")
-	public List<Student> GetStudentList() {
-		return repository.studentListsearch();
-	}
-
-	@GetMapping("studentCourseList")
-	public List<StudentCourse> GetStudentCourseList() {
-		return repository.studentCourseListsearch();
-	}
-
 }
---
+++ src/main/java/raisetech/Student/Management/StudentController.java
@@ -0,0 +1,25 @@
+package raisetech.Student.Management;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+  private final StudentRepository repository;
+
+  public StudentController(StudentRepository repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping("/studentList")
+  public List<Student> getStudentList() {
+    return repository.studentListsearch();
+  }
+
+  @GetMapping("/studentCourseList")
+  public List<StudentCourse> getStudentCourseList() {
+    return repository.studentCourseListsearch();
+  }
+}
